320e437b2709c31a61300afbd3192e8c71ffdce3
Ranusha Senesh Akmeemana <seneshak2001@gmail.com>
2024-08-17 20:59:01 +0530

    Removed breaks. Taking the row up.

--- resources/views/posts/view.blade.php
+++ resources/views/posts/view.blade.php
@@ -4,11 +4,9 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     <h1 class="text-3xl font-semibold mb-4">{{ $post->title }}</h1>
-                    <br>
                     @if($post->image)
                         <img src="{{ asset('storage/'.$post->image) }}" alt="{{ $post->title }}" class="mb-4 rounded-md" style="max-width: 100%;">
                     @endif
-                    <br>
                     <p>{{ $post->text }}</p>
                     <div class="text-gray-700 space-y-6">
                         {!! nl2br(e($post->content)) !!}
